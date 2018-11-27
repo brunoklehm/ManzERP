e6aabbe2037494f9389d0e91a6bc0689ed2f92f1
reblinsegundo <caioreblin07@gmail.com>
2018-11-27 21:17:59 -0200

    criação do banco

---
+++ banco/create.sql
@@ -0,0 +1,22 @@
+create database manzerp;
+use manzerp;
+
+create table usuario(
+id int not null primary key auto_increment,
+nome varchar(50) not null,
+cpf char(11) not null,
+tipo int not null,
+login varchar(20) not null,
+senha varchar(20) not null
+);
+
+create table chamado(
+id int not null primary key auto_increment,
+tipo int not null,
+descricao varchar(100) not null,
+status int not null,
+usuario_solicitante int not null,
+usuario_atendente int,
+foreign key (usuario_solicitante) references usuario(id) on update cascade on delete cascade,
+foreign key (usuario_atendente) references usuario(id) on update cascade on delete cascade
+);
