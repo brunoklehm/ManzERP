49959d6064d774502b2cff6f514b867b973db6c4
reblinsegundo <caioreblin07@gmail.com>
2018-11-29 11:56:44 -0200

    updated sql

--- banco/create.sql
+++ banco/create.sql
@@ -14,11 +14,12 @@ status int not null
 create table chamado(
 id int not null primary key auto_increment,
 tipo int not null,
+data_solicitacao varchar(10) not null,
 descricao varchar(100) not null,
 nome varchar(50) not null,
 status int not null,
 usuario_solicitante int not null,
 usuario_atendente int,
-foreign key (usuario_solicitante) references usuario(id) on update cascade on delete cascade,
-foreign key (usuario_atendente) references usuario(id) on update cascade on delete cascade
+foreign key (usuario_solicitante) references usuario(id) on update cascade,
+foreign key (usuario_atendente) references usuario(id) on update cascade,
 );
